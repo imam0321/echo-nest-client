12fc63e5f98da18532ce6534483dede3b37940f6
imam0321 <imam.hossain0321@gmail.com>
2025-01-29 01:56:21 +0600

    add: login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,48 @@
-
+import { Link } from "react-router-dom";
+import LoginImage from "../../assets/images/auth_illustration.png";
+import LoginForm from "./LoginForm";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <main className="flex min-h-screen items-center justify-center py-8">
+        <div className="max-w-[1268px] flex-1">
+          <div className="container grid items-center gap-8 lg:grid-cols-2">
+            <div>
+              <img
+                className="mb-12 max-w-full max-lg:hidden"
+                src={LoginImage}
+                alt="An illustration of user authentication"
+              />
+              <div>
+                <h1 className="mb-3 text-4xl font-bold lg:text-[40px]">
+                  Echo Nest
+                </h1>
+                <p className="max-w-[452px] text-gray-600/95 lg:text-lg">
+                  Create a social media app with features like showing posts,
+                  post details, reactions, comments, and profiles.
+                </p>
+              </div>
+            </div>
+            <div className="card">
+
+              <LoginForm />
+
+              <div className="py-4 lg:py-6">
+                <p className="text-center text-xs text-gray-600/95 lg:text-sm">
+                  Don’t have an account?{" "}
+                  <Link
+                    className="text-white transition-all hover:text-green-500 hover:underline"
+                    to="/signUp"
+                  >
+                    Create New
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
---
+++ src/pages/Login/LoginForm.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import Field from '../Share/Fields/Field';
+import { useNavigate } from 'react-router-dom';
+
+export default function LoginForm() {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+    navigate("/")
+  }
+
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      onSubmit={handleSubmit((submitForm))}
+    >
+
+      <Field label="Email" error={errors.email}>
+        <input
+          {...register("email", { required: "Email is Required" })}
+          type="email"
+          name="email"
+          id="email"
+          className={`w-full rounded-md border p-1.5 focus:outline-none lg:p-3 ${errors.email ? "border-red-500" : "border-[#CCCCCC]/[14%]"}`}
+        />
+      </Field>
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", { required: "Password id Required", minLength: { value: 8, message: "Your password must be at lest 8 character" } })}
+          type="password"
+          name='password'
+          className={`w-full rounded-md border p-1.5 focus:outline-none lg:p-3 ${errors.password ? "border-red-500" : "border-[#CCCCCC]/[14%]"}`} />
+      </Field>
+      <Field>
+        <button
+          className="auth-input bg-[#00D991] font-bold text-[#17181C] transition-all hover:opacity-90"
+          type="submit"
+        >
+          Login
+        </button>
+      </Field>
+    </form>
+  )
+}
