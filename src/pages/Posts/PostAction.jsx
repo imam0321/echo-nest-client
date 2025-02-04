295380d35df88a9e18b2f719453b750a1ae7fbaf
imam0321 <imam.hossain0321@gmail.com>
2025-02-05 01:13:32 +0600

    add: implement like functionality in PostAction component and enhance PostComments with comment submission logic

--- src/pages/Posts/PostAction.jsx
+++ src/pages/Posts/PostAction.jsx
@@ -1,15 +1,36 @@
 import LikeIcon from "../../assets/icons/like.svg"
 import CommentIcon from "../../assets/icons/comment.svg"
 import ShareIcon from "../../assets/icons/share.svg"
+import Liked from "../../assets/icons/like-filled.svg"
+import { useState } from "react"
+import useAxios from "../../hooks/useAxios";
+import useAuth from "../../hooks/useAuth"
 
-export default function PostAction({ postId, commentCount }) {
+export default function PostAction({ post, commentCount }) {
+  const { auth } = useAuth();
+  const [liked, setLiked] = useState(post?.likes?.includes(auth?.user?.id));
+  const { api } = useAxios();
+
+  const handleLiked = async () => {
+    try {
+      const response = await api.patch(`${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post?.id}/like`)
+      if (response.status === 200) {
+        setLiked(true)
+      }
+    } catch (error) {
+      console.log(error);
+      setLiked(false)
+    }
+  }
+
   return (
     <div className="flex items-center justify-between py-6 lg:px-10 lg:py-8">
       <button
+        onClick={handleLiked}
         className="flex-center gap-2 text-xs font-bold text-[#B8BBBF] hover:text-white lg:text-sm"
       >
-        <img src={LikeIcon} alt="Like" />
-        <span>Like</span>
+        <img className="w-6" src={liked ? Liked : LikeIcon} alt="Like" />
+        {!liked && <span>Like</span>}
       </button>
 
       <button
--- src/pages/Posts/PostComments.jsx
+++ src/pages/Posts/PostComments.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react'
-import useAvatar from '../../hooks/useAvatar';
 import BlankProfile from "../../assets/images/blankProfile.jpeg"
+import useAuth from '../../hooks/useAuth';
+import useAxios from '../../hooks/useAxios';
 
 export default function PostComments({ post }) {
+  const { auth } = useAuth();
+  const { api } = useAxios();
   const [showAction, setShowAction] = useState(false);
-  const { avatarURL } = useAvatar(post);
-  const comments = post?.comments;
+  const [comments, setComments] = useState(post?.comments);
+  const [comment, setComment] = useState("");
+
+  const addComment = async (e) => {
+    const keyCode = e.keyCode;
+
+    if (keyCode === 13) {
+      try {
+        const response = await api.patch(`${import.meta.env.VITE_SERVER_BASE_URL}/posts/${post?.id}/comment`, { comment })
+        if (response.status === 200) {
+          setComments([...response?.data?.comments])
+          setComment("")
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
 
   return (
     <div>
       <div className="flex-center mb-3 gap-2 lg:gap-4">
         <img
           className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-          src={avatarURL || BlankProfile}
+          src={auth?.user?.avatar ? `${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}` : BlankProfile}
           alt="avatar"
         />
 
@@ -22,6 +41,9 @@ export default function PostComments({ post }) {
             className="h-8 w-full rounded-full bg-[#27292F] px-4 text-xs focus:outline-none sm:h-[38px]"
             name="post"
             id="post"
+            value={comment}
+            onChange={e => setComment(e.target.value)}
+            onKeyDown={e => addComment(e)}
             placeholder="What's on your mind?"
           />
         </div>
@@ -34,7 +56,7 @@ export default function PostComments({ post }) {
       {
         showAction && <div className="space-y-4 divide-y divide-[#27292F] pl-2 lg:pl-3">
           {
-            comments.map(comment => <div key={comment?.id} className="flex items-center gap-3 pt-4">
+            comments.map((comment, index) => <div key={index} className="flex items-center gap-3 pt-4">
               <img
                 className="max-w-6 max-h-6 rounded-full"
                 src={comment?.author?.avatar ? `${import.meta.env.VITE_SERVER_BASE_URL}/${comment?.author?.avatar}` : BlankProfile}
